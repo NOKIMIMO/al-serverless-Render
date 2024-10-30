dd18a0702d7dd771585ac9753d73e9e6b0c71e8d
NOKIMIMO <janikowski.nicolas@gmail.com>
2024-10-30 10:43:53 +0100

    add user

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const logger = require('morgan')
 const sassMiddleware = require('node-sass-middleware')
 
 const indexRouter = require('./routes/index')
-// const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
-
+console.log("url db :",process.env.DATABASE_URL);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
@@ -27,7 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-// app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
